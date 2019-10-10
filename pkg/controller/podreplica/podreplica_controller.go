3bca4832431d837a374007404720e066170989dc
Manuel <manuel.wessner@codecentric.de>
2019-10-10 16:40:09 +0200

    :sparkles: Implement Scaling

--- pkg/controller/podreplica/podreplica_controller.go
+++ pkg/controller/podreplica/podreplica_controller.go
@@ -2,13 +2,14 @@ package podreplica
 
 import (
 	"context"
+	"reflect"
 
 	appv1alpha1 "github.com/wtfjoke/pod-replica/pkg/apis/app/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -20,11 +21,6 @@ import (
 )
 
 var log = logf.Log.WithName("controller_podreplica")
-
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
 
 // Add creates a new PodReplica Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
@@ -77,18 +73,16 @@ type ReconcilePodReplica struct {
 
 // Reconcile reads that state of the cluster for a PodReplica object and makes changes based on the state read
 // and what is in the PodReplica.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcilePodReplica) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling PodReplica")
+	reqLogger.Info("⚖️ Reconciling PodReplica")
 
 	// Fetch the PodReplica instance
-	instance := &appv1alpha1.PodReplica{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	podReplica := &appv1alpha1.PodReplica{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, podReplica)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
@@ -100,33 +94,68 @@ func (r *ReconcilePodReplica) Reconcile(request reconcile.Request) (reconcile.Re
 		return reconcile.Result{}, err
 	}
 
-	// Define a new Pod object
-	pod := newPodForCR(instance)
+	// Fetch existing replica pods
+	existingReplicaPods := &corev1.PodList{}
+	existingReplicaPodsQuery := &client.ListOptions{
+		Namespace:     request.Namespace,
+		LabelSelector: labels.SelectorFromSet(labelsForPodReplicas(request.Name)),
+	}
 
-	// Set PodReplica instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
+	err = r.client.List(context.TODO(), existingReplicaPodsQuery, existingReplicaPods)
+	if err != nil {
+		reqLogger.Error(err, "💥 Failed to list existing pods in the podreplica")
 		return reconcile.Result{}, err
 	}
 
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
+	runningReplicaPodNames := getRunningReplicaPodNames(existingReplicaPods.Items)
+
+	// Update pod names (in status)
+	if !reflect.DeepEqual(podReplica.Status.Replicas, runningReplicaPodNames) {
+		podReplica.Status.Replicas = runningReplicaPodNames
+		err := r.client.Update(context.TODO(), podReplica)
+		if err != nil {
+			reqLogger.Error(err, "💥 Failed to update podreplica")
+			return reconcile.Result{}, err
+		}
+	}
+
+	reqLogger.Info("👀 Checking podreplicas", "expected replicas", podReplica.Spec.Size, "Pod.Names", runningReplicaPodNames)
+
+	// scale down replicas
+	if int32(len(runningReplicaPodNames)) > podReplica.Spec.Size {
+		podToBeDeleted := existingReplicaPods.Items[0]
+		reqLogger.Info("💀 Deleting a pod", "Pod.Namespace", podToBeDeleted.Namespace, "Pod.Name", podToBeDeleted.Name)
+		err := r.client.Delete(context.TODO(), &podToBeDeleted)
+		if err != nil {
+			reqLogger.Error(err, "💥 Failed to delete a pod")
+			return reconcile.Result{}, err
+		}
+	}
+
+	// scale up replicas
+	if int32(len(runningReplicaPodNames)) > podReplica.Spec.Size {
+		// Define a new Pod object
+		pod := newPodForCR(podReplica)
+		reqLogger.Info("👶 Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
 		err = r.client.Create(context.TODO(), pod)
 		if err != nil {
+			reqLogger.Error(err, "💥 Failed to create a pod")
 			return reconcile.Result{}, err
 		}
 
-		// Pod created successfully - don't requeue
-		return reconcile.Result{}, nil
-	} else if err != nil {
-		return reconcile.Result{}, err
+		// Set PodReplica instance as the owner and controller
+		if err := controllerutil.SetControllerReference(podReplica, pod, r.scheme); err != nil {
+			return reconcile.Result{}, err
+		}
 	}
 
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
-	return reconcile.Result{}, nil
+	if int32(len(runningReplicaPodNames)) == podReplica.Spec.Size {
+		reqLogger.Info("⏭️ Skip reconcile: Enough replicas")
+		return reconcile.Result{}, nil
+	}
+
+	// requeue for additional scaling or updating pod names
+	return reconcile.Result{Requeue: true}, nil
 }
 
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
@@ -150,4 +179,21 @@ func newPodForCR(cr *appv1alpha1.PodReplica) *corev1.Pod {
 			},
 		},
 	}
+}
+
+func labelsForPodReplicas(name string) map[string]string {
+	return map[string]string{"app": name}
+}
+
+func getRunningReplicaPodNames(existingReplicaPods []corev1.Pod) []string {
+	runningReplicaPodNames := []string{}
+	for _, replicaPod := range existingReplicaPods {
+		if replicaPod.DeletionTimestamp != nil { // ignore pods beeing terminated
+			continue
+		}
+		if replicaPod.Status.Phase == corev1.PodPending || replicaPod.Status.Phase == corev1.PodRunning {
+			runningReplicaPodNames = append(runningReplicaPodNames, replicaPod.Name)
+		}
+	}
+	return runningReplicaPodNames
 }
